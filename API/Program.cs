0cb4dacbf5208c5e77040d30a1126da07da3a9a9
Adina <adina_miki2008@yahoo.com>
2021-03-13 13:49:39 +0200

    Add PaymentsComtroller

--- API/Program.cs
+++ API/Program.cs
@@ -18,11 +18,11 @@ namespace API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope()) 
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                try 
+                try
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
@@ -33,7 +33,7 @@ namespace API
                     await identityContext.Database.MigrateAsync();
                     await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
                 }
-                catch(Exception e) 
+                catch(Exception e)
                 {
                     var logger = loggerFactory.CreateLogger<Program>();
                     logger.LogError(e, "An error occured during migration");
